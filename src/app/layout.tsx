690a906f69b5ff3eac515fe10dcd7c009662ebc3
flofehrenbacher-sz <florian.fehrenbacher@sz.de>
2023-04-01 12:22:04 +0200

    feat: add apple touch icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="manifest" href="manifest.json" />
+        <link rel="apple-touch-icon" href="images/apple-touch-icon-iphone-60x60.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="images/apple-touch-icon-ipad-76x76.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="images/apple-touch-icon-iphone-retina-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="images/apple-touch-icon-ipad-retina-152x152.png"
+        />
       </head>
       <body>{children}</body>
     </html>
